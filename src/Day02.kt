feb04d3e40df6e9ff863f48757faf53a0e01b003
vivek panchal <vivekpanchal64@gmail.com>
2022-12-02 09:52:59 +0530

    moved input to another package

---
+++ src/Day02.kt
@@ -0,0 +1,16 @@
+
+
+fun main() {
+    fun part1(input: List<String>): Int {
+
+        return 1
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
